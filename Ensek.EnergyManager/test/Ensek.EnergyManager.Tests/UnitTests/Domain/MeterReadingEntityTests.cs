de1f7fa228bd9c3dd746892708cdca92d9da51de
Paul Charlton <paulmcharlton@gmail.com>
2022-08-03 17:39:59 +0100

    MeterReadingEntityTests

--- Ensek.EnergyManager/test/Ensek.EnergyManager.Tests/UnitTests/Domain/MeterReadingEntityTests.cs
+++ Ensek.EnergyManager/test/Ensek.EnergyManager.Tests/UnitTests/Domain/MeterReadingEntityTests.cs
@@ -1,27 +1,41 @@
-﻿namespace Ensek.EnergyManager.Tests.UnitTests.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ensek.EnergyManager.Tests.UnitTests.Domain;
 
 public class MeterReadingEntityTests
 {
+    private readonly AccountEntity _account;
+
+    public MeterReadingEntityTests()
+    {
+        _account = new AccountEntity("12345", new Name("Test", "Test"));
+    }
 
     [Fact]
     public void Constructor_WithNegativeMeterReading_ThrowsException()
     {
-        true.Should().BeFalse();
         // SETUP
+        var reading = -556;
 
         // TEST
+        var action = () => new MeterReadingEntity(_account, DateTimeOffset.Now, reading);
 
         // ASSERT
+        action.Should().Throw<ValidationException>();
     }
+
     [Fact]
     public void Constructor_WithValidMeterReading_ExecutesSuccessfully()
     {
-        true.Should().BeFalse();
         // SETUP
+        var reading = 556;
 
         // TEST
+        var meter = new MeterReadingEntity(_account, DateTimeOffset.Now, reading);
 
         // ASSERT
+        meter.Should().NotBeNull();
+        meter.MeterReadingValue.Should().Be(reading);
     }
 
 }
