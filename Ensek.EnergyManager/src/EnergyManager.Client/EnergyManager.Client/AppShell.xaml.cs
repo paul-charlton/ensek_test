5e7cebcfdf844b9e4b80865c802dc95b20e97193
Paul Charlton <paulmcharlton@gmail.com>
2022-08-04 06:31:37 +0100

    initial client check in

---
+++ Ensek.EnergyManager/src/EnergyManager.Client/EnergyManager.Client/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using EnergyManager.Client.ViewModels;
+using EnergyManager.Client.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EnergyManager.Client
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Ensek.EnergyManager/src/EnergyManager.Client/EnergyManager.Client/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EnergyManager.Client.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace EnergyManager.Client.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
